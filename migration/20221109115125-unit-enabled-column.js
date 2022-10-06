46589406479662766d57b4edd3170f28989ea10c
Rama Mahjoub <ramamhrama093@gmail.com>
2022-10-06 17:44:24 +0300

    #2

---
+++ migration/20221109115125-unit-enabled-column.js
@@ -0,0 +1,23 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class unitEnabledColumn20221109115125 {
+
+    async up(queryRunner) {
+        await queryRunner.query(
+            `
+            create table \`unit\` (
+                \`id\` char(36) not null,
+                \`name\` varchar(50) not null,
+                \`code\` varchar(10)  null
+              ) Engine=InnoDB;
+              `,
+          );
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(
+            `drop table \`unit\`;`,
+          );
+    }
+
+}
