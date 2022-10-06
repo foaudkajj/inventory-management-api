b60fae4bb9f4928c009be6924b7a09e03b71ea15
Rama Mahjoub <ramamhrama093@gmail.com>
2022-10-06 19:56:18 +0300

    #6

---
+++ migration/20221109115125-currency-table.js
@@ -0,0 +1,19 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class currencyTable20221109115125 {
+
+    async up(queryRunner) {
+        await queryRunner.query(
+            `
+        create table \`currency\` (
+            \`id\` char(36) not null,
+            \`name\` varchar(50) not null
+          ) Engine=InnoDB;
+          `,
+        );
+    }
+
+    async down(queryRunner) {
+    }
+
+}
