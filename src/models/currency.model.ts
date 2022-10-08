7d7fa918380ced8b2c60f0b3ca89eaf99866a6d9
Rama Mahjoub <ramamhrama093@gmail.com>
2022-10-08 13:53:02 +0300

    #12

--- src/models/currency.model.ts
+++ src/models/currency.model.ts
@@ -1,5 +1,6 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
+import { TransactionCard } from './transaction-card.model';
 
 @Entity()
 export class Currency {
@@ -10,4 +11,7 @@ export class Currency {
   @Column({length: 50, name: 'name'})
   @ApiProperty({required: true, type: 'string'})
   name: string;
+
+  @OneToMany(() => TransactionCard, (transactionCard) => transactionCard.currency)
+  transactionCards: TransactionCard[];
 }
