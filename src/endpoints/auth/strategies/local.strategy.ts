3e37d6193b6e7c5af69bf92686b3d7aff41719ed
Rama Mahjoub <ramamhrama093@gmail.com>
2022-10-12 19:36:28 +0300

    #21

--- src/endpoints/auth/strategies/local.strategy.ts
+++ src/endpoints/auth/strategies/local.strategy.ts
@@ -10,7 +10,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(username: string, password: string) {
-        console.log('LocalStrategy ');
         const user = await this.authService.validateUser(username, password);
         if (!user) {
             throw new UnauthorizedException();
