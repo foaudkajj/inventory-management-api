71abebd87fce1e3f1bbea752c34aa8324a47735f
Rama Mahjoub <ramamhrama093@gmail.com>
2022-10-12 20:28:11 +0300

    #22

--- migration/20221011164525-insert-countries-to-db.js
+++ migration/20221011164525-insert-countries-to-db.js
@@ -4,7 +4,7 @@ module.exports = class insertCountriesToDb20221011164525 {
 
     async up(queryRunner) {
         await queryRunner.query(
-            `insert into \`country\` (\`id\`,\`name\`) values ('3fa85f64-5717-4562-b3fc-2c963f66afa7','country 1');`
+            `insert into \`country\` (\`id\`,\`name\`) values ('01a88864-b14e-41d5-86ed-110ffbd6c2ce','Turkey');`
         );
     }
 
