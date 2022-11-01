f3c5e14958e887419a3cc50c8d785f593b677150
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-11-01 09:32:37 +0300

    login-response

---
+++ src/models/responses/login.response.ts
@@ -0,0 +1,22 @@
+import {ApiProperty} from '@nestjs/swagger';
+import {UserStatus} from '../user.model';
+
+export class LoginResponse {
+  @ApiProperty({type: 'token'})
+  token: string;
+
+  @ApiProperty({type: 'string'})
+  username: string;
+
+  @ApiProperty({enum: UserStatus})
+  status: UserStatus;
+
+  @ApiProperty({type: 'string'})
+  firstName: string;
+
+  @ApiProperty({type: 'string'})
+  lastName: string;
+
+  @ApiProperty({type: 'string'})
+  role: string;
+}
