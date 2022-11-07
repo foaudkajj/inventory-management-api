92391d25a7d5a062f8a784fbfe8af177549ac864
Rama Mahjoub <ramamhrama093@gmail.com>
2022-11-07 16:40:37 +0200

    #45

--- src/endpoints/user/user.service.ts
+++ src/endpoints/user/user.service.ts
@@ -1,38 +1,54 @@
-import {Injectable} from '@nestjs/common';
-import {genSaltSync, hashSync} from 'bcryptjs';
-import {User} from 'src/models';
-import {UserRepository} from './user.repository';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { genSaltSync, hashSync } from 'bcryptjs';
+import { User } from 'src/models';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private userRepository: UserRepository) { }
   getAll(): Promise<User[]> {
     return this.userRepository.orm.find();
   }
 
-  insert(row: User) {
+  async insert(row: User) {
+    const usernameExist = await this.userRepository.orm.findOneBy({ username: row.username });
+    if (usernameExist) {
+      throw new HttpException(
+        'ERROR.USERNAME_ALREADY_EXIST',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     const salt = genSaltSync();
     const hashpassword = hashSync(row.password, salt);
     row.password = hashpassword;
     return this.userRepository.orm.insert(row);
   }
 
-  update(row: Partial<User>, id: string) {
+  async update(row: Partial<User>, id: string) {
     if (row.password) {
       const salt = genSaltSync();
       const hashpassword = hashSync(row.password, salt);
       row.password = hashpassword;
     }
-    return this.userRepository.orm.update({id: id}, row);
+    if (row.username) {
+      const usernameExist = await this.userRepository.orm.findOneBy({ username: row.username });
+      if (usernameExist) {
+        throw new HttpException(
+          'ERROR.USERNAME_ALREADY_EXIST',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    }
+    return this.userRepository.orm.update({ id: id }, row);
   }
 
   delete(id: string) {
-    return this.userRepository.orm.delete({id: id});
+    return this.userRepository.orm.delete({ id: id });
   }
 
   findOneByUsername(username: string) {
     return this.userRepository.orm.findOne({
-      where: {username},
+      where: { username },
       relations: {
         role: {
           rolePermissions: {
